7bb52d66734ce2c13420b5c56939e138d642520b
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-02-28 06:29:49 -0500

    Update main1.py

--- main1.py
+++ main1.py
@@ -4,6 +4,6 @@ response = os.system("ping -c 1 " + hostname)
 
 #and then check the response...
 if response == 0:
-  print hostname, 'is up!'
+  print(hostname, 'is up!')
 else:
-  print hostname, 'is down!'
+  print(hostname, 'is down!')
