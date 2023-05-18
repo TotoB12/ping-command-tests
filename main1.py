cd1ca6690d32748ea4c2dc7be1267dc30128f033
Sourcery AI <>
2023-05-18 01:47:46 +0000

    'Refactored by Sourcery'

--- main1.py
+++ main1.py
@@ -1,6 +1,6 @@
 import os
 hostname = "google.com" #example
-response = os.system("ping -c 1 " + hostname)
+response = os.system(f"ping -c 1 {hostname}")
 
 #and then check the response...
 if response == 0:
